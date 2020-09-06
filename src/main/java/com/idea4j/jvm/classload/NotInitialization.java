029e8951bb685b0e274a8d528de85a48efa55a19
beijinguang <wangjinguang66@163.com>
2020-09-06 23:24:05 +0800

    类的加载过程

--- src/main/java/com/idea4j/jvm/classload/NotInitialization.java
+++ src/main/java/com/idea4j/jvm/classload/NotInitialization.java
@@ -2,6 +2,9 @@ package com.idea4j.jvm.classload;
 
 /**
  * -XX:+TraceClassLoading
+ * 非主动使用类字段
+ * 对于静态字段，只有定义这个类的字段才会被初始化，子类引用父类的静态字段
+ * 不会触发子类的初始化
  */
 public class NotInitialization {
     public static void main(String[] args) {
